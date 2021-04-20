ade2a31fcd717f93f174afbef64cee8428a1e4fb
dmltkd1974 <dmltkd1974@naver.com>
2021-04-20 09:52:54 +0900

    -

--- simpleboard/src/main/java/com/manong/board/repository/BoardRepository.java
+++
@@ -1,9 +0,0 @@
-package com.manong.board.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.manong.board.domain.Board;
-
-public interface BoardRepository extends JpaRepository<Board, Long>{
-	
-}
